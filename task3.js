7f05a1743c94e457233b7d7aa11f56c2b209660a
KimSombra <166673820+KimSombra@users.noreply.github.com>
2024-05-02 09:25:28 +0800

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,31 @@
+<!doctype html>
+
+	<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>JavaScript Arithmetic Operators using Buttons</title>
+		<script type="text/javascript">
+			function fun1()
+			{
+				var a=parselnt(t1.value);
+				var b=parselnt(t2.value);
+				var c=a+b;
+				document.getElementByld("output").innerHTML="Sum ="+c;
+
+
+			}
+		</script>
+
+	</head>
+	<body>
+		<tr>
+
+		<td>Enter 1st No.:</td>
+		<td><input type="text" id="t1"/></td>
+	</tr>
+	<tr>
+		<td colspan></td>
+	</tr>
+	</body>
+	</html>
